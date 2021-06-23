8c09a1d80a5bcae4a0f90395eea549921516113f
ThomasBeckDresden <thomas.beck.dresden@gmail.com>
2021-06-23 10:17:22 +0200

    initial trixx that came up

---
+++ trixx.js
@@ -0,0 +1,27 @@
+// Extensions that are useful:
+
+// Auto Rename Tag
+// Bracket Pair Colorizer
+// ESLint;
+// indent-rainbow
+// JavaScript (ES6) Code Snippets
+// Prettier - Code Formatter
+// Quokka.js
+
+// Shortcuts that are useful:
+
+// <command> + <shift> + <p> => opens VsCode command pallete at the top
+// <command> + <shift> + <F2> => triggers an option on a tag which will rename all appearances of the string highlighted (e.g to change all appearances of the string "class" to "className" )
+// <command> + <K> + <C> => comment the selected part of the code (automatically uses the correct syntax according to the language in use)
+// <command> + <K> + <U> => uncomment the selected part of the code (automatically uses the correct syntax according to the language in use)
+// <alt> + <Z> => toggle word wrap
+
+// <command />
+
+// user preferences that may be necessary:
+
+// access via <command> + <,>
+
+// editor: tabsize (may be changed for indent-rainbow extension showing every first indent in red)
+
+// extensions: emmet: include languages: item: "javascript" value: "reactjavascript" (for me emmet didn't work until I did this)
